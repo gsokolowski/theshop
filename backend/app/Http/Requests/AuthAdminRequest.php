abe0f660a3995fd6597e13d5f7966cb3ede50a1b
Greg <greg@mac.home>
2025-12-18 02:18:45 +0100

    Admin login log out ready

--- backend/app/Http/Requests/AuthAdminRequest.php
+++ backend/app/Http/Requests/AuthAdminRequest.php
@@ -11,7 +11,7 @@ class AuthAdminRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class AuthAdminRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'email' => 'required|email|exists:admins,email|max:255', // validate email exists in admins table
+            'password' => 'required|min:6|max:255',
         ];
     }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,16 +3,12 @@
 use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // General route to login form
 Route::get('/',[AdminController::class,"loginForm"])->name("admin.loginForm");
 Route::post('/admin/auth',[AdminController::class,"auth"])->name("admin.auth");
 
 // Admin routes with middleware admin   
 Route::middleware('admin')->prefix('admin')->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
     Route::post('/logout', [AdminController::class, 'logout'])->name('admin.logout');
 });
