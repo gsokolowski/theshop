ba0ffc7d8f8b62fd00b5d0245321b4c9cad942cf
gsokolowski <gregor.sokolowski@gmail.com>
2025-12-23 01:08:16 +0100

    Category index create edit delete in Dashboard done

---
+++ backend/app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // return view with all latest categories sorted by created_at in descending order
+        return view('admin.categories.index', [
+            'categories' => Category::latest()->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // Show the form for creating a new category
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryStoreRequest $request)
+    {
+        // Validate the CategoryStoreRequest
+        if($request->validated()) {
+            // Create a new category
+
+            $data = $request->validated();
+            $data['slug'] = Str::slug($data['name']);
+            Category::create($data);
+            // Redirect to the index page with a success message
+            return redirect()->route('admin.categories.index')->with('success', 'Category created successfully');
+        } else {
+            // Redirect to the create page with a error message
+            return redirect()->route('admin.categories.create')->with('error', 'Category creation failed');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        // dont show the show view for a category
+        abort(404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        // Show the form for editing category use with() to pass the category
+        return view('admin.categories.edit', [
+            'category' => $category
+        ]);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryUpdateRequest $request, Category $category)
+    {
+        // Validate the CategoryUpdateRequest use slug
+        if($request->validated()) {
+            // Update the category
+            $data = $request->validated();
+            $data['slug'] = Str::slug($data['name']);
+            $category->update($data);
+            // Redirect to the index page with a success message
+            return redirect()->route('admin.categories.index')
+                ->with('success', 'Category updated successfully');                
+        } else {
+            // Redirect to the edit page with a error message
+            return redirect()->route('admin.categories.edit', $category->id)
+                ->with('error', 'Category update failed');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        // Delete the category
+        $category->delete();
+        // Redirect to the index page with a success message
+        return redirect()->route('admin.categories.index')
+            ->with('success', 'Category deleted successfully');
+    }
+}
---
+++ backend/resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,48 @@
+@extends('admin.layouts.app')
+@section('title', 'Categories')
+@section('content-dashboard')
+    <div class="row">
+        @include('admin.layouts.sidebar')
+        <div class="col-md-9">
+        <div class="row mt-2">
+            <div class="col-12">
+                <div class="card-header bg-white d-flex justify-content-between align-items-center">
+                    <h3 class="mt-2">
+                        Categories
+                    </h3>
+                    <a href="{{route('admin.categories.create')}}" class="btn btn-sm btn-primary">
+                        <i class="fas fa-plus"></i>
+                    </a>
+                </div>
+                <hr>
+                <!--  form for create category -->
+                <form action="{{route('admin.categories.update', $category->slug)}}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input 
+                        type="text" 
+                        class="form-control" @error('name') is-invalid @enderror
+                        name ="name"
+                        id="name"
+                        class="form-control" 
+                        required 
+                        value="{{$category->name}}"
+                        placeholder="Enter category name"
+                        autofocus>
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-sm btn-primary">
+                        <i class="fas fa-edit"></i>
+                        Update
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
