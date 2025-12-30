7145ec358e44ec250662e1e11c98b20b0aec2f36
Greg <greg@mac.home>
2025-12-30 02:47:02 +0100

    first api call for products

---
+++ frontend -basic-router-api-call/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <Navbar />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+  import Navbar from './components/layouts/Navbar.vue'
+</script>
+
+
+<style scoped>
+</style>
