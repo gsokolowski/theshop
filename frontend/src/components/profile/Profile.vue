73ff1003e8394eb8436e9907687feda2f09ab700
gsokolowski <gregor.sokolowski@gmail.com>
2026-01-13 19:58:39 +0100

    ValidationErrors and vaidattion messages seperatelly

--- frontend/src/components/profile/Profile.vue
+++ frontend/src/components/profile/Profile.vue
@@ -1,32 +1,13 @@
 <template>
-  <div>
-    <h1>Profile</h1>
-    <Spinner :store="authStore" />
+  <div class="row min-vh-100">
+      <ProfileSidebar/>
+      <ProfileUpdate/>
   </div>
 </template>
 
 <script setup>
-import { useAuthStore } from '../../stores/useAuthStore'    
-import { onMounted } from 'vue'
-import Spinner from '../layouts/Spinner.vue'
-
-const authStore = useAuthStore()
-
-
-
-
-onMounted( async () => {
-    // clear validation error messages in the authStore
-    authStore.setValidationErrors({})
-    authStore.setValidationMessage('')
-    try {
-        const user = await authStore.getLoggedInUser();               
-        console.log(user)
-    } catch (error) {
-        console.error('Profile error:', error)
-    }
-})
-
+  import ProfileSidebar from './ProfileSidebar.vue'
+  import ProfileUpdate from './ProfileUpdate.vue'
 </script>
 
 <style scoped>
